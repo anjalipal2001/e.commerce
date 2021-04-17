03133a5c878f747c1b5ee8632a3365eace4608a8
anjalipal2001 <ap7197339@gmail.com>
2021-04-17 22:31:47 +0530

    Updates

---
+++ precache-manifest.74edfed5c473689db793ea4fcb0f8e64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46b90321112caacdfd8b9fc3f44c8236",
+    "url": "/e.commerce/index.html"
+  },
+  {
+    "revision": "2fa4de7555a8158b3bdb",
+    "url": "/e.commerce/static/css/main.8ce49167.chunk.css"
+  },
+  {
+    "revision": "76100f30648f6a3c2377",
+    "url": "/e.commerce/static/js/2.1c23f1f4.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/e.commerce/static/js/2.1c23f1f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fa4de7555a8158b3bdb",
+    "url": "/e.commerce/static/js/main.bb2135a5.chunk.js"
+  },
+  {
+    "revision": "0038858d22b09bd2ff38",
+    "url": "/e.commerce/static/js/runtime-main.6b864c17.js"
+  }
+]);
